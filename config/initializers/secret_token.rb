99cc400f8a803d70f3fade5aded393f4bb628a09
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-03-12 11:17:20 -0500

    Installed rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuftsElection::Application.config.secret_token = 'df03b1e8450b954dde643ca952cb2e9d610fdbf951f4a5c59b45f0677d9b6f46868f16f63c305a024e19db7ba6f4ae07366b5fdf6e0b1f808ec72091769b4e15'
