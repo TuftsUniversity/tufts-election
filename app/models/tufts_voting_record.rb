3ce654e47db01bfbd349c2a39c839bc099b4cba1
Mike Korcynski <mkorcy@gmail.com>
2012-06-20 14:38:48 -0400

    vm76 reports TuftsVotingRecord for class.to_s, prod reports ActiveFedora::Base. either something changed in solrizer or rubydora those are the only gems that drifted versions

--- app/models/tufts_voting_record.rb
+++ app/models/tufts_voting_record.rb
@@ -2,7 +2,7 @@ class TuftsVotingRecord < ActiveFedora::Base
   has_metadata :name => "RECORD-XML", :type => Datastreams::ElectionRecord
   has_metadata :name => "DCA-META", :type => Datastreams::TuftsDcaMeta
   
-  def to_solr(solr_doc=Hash.new)
+  def to_solr(solr_doc=Hash.new,opt={})
     super
     solr_doc["title_display"] = solr_doc["title_sort"] = solr_doc["title_t"].first
     solr_doc["party_affiliation_facet"] = datastreams["RECORD-XML"].office.role.ballot.candidate.affiliation.to_a + datastreams["RECORD-XML"].office.role.ballot.elector.affiliation.to_a 
