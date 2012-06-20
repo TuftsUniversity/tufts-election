da7e9d07b0141d7dd9dade01e5f0cee72e36c064
Mike Korcynski <mkorcy@gmail.com>
2012-06-20 15:01:38 -0400

    removing the cookie cleaning code as it doesn't seem to be necessary on this machine

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.configuration.middleware.insert_before ActionDispatch::Cookies, CleanCookies::Rack
+#Rails.configuration.middleware.insert_before ActionDispatch::Cookies, CleanCookies::Rack
 TuftsElection::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
