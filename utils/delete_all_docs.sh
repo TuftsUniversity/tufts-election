558004c62eb0cb4a8693174dadf0fbf6d395d1c9
Mike Korcynski <mkorcy@gmail.com>
2012-06-26 15:18:02 -0400

    some useful util scripts

---
+++ utils/delete_all_docs.sh
@@ -0,0 +1,2 @@
+java -Durl=http://mhf-dev-01.lib.tufts.edu/solr_tufts_election/tufts-election/update -Ddata=args -Dcommit=no -jar post.jar "<delete><query>*:*</query></delete>"
+
---
+++ utils/run_solrizerd.sh
@@ -0,0 +1,2 @@
+cd /home/hydradm/tufts-election
+solrizerd start --hydra_home /home/hydradm/tufts-election/ -p 61613 -o dev-tdlamq.lib.tufts.edu
