566b5ea1967a549d4848c635df259df1a04b03ef
Mike Korcynski <mkorcy@gmail.com>
2014-04-28 14:51:15 -0400

    updated solr fields for latest af

--- app/helpers/candidate_helper.rb
+++ app/helpers/candidate_helper.rb
@@ -1,9 +1,9 @@
 module CandidateHelper
   def list_elections 
-    docs = Blacklight.solr.get('select', :params=>{:q=>"(candidate_id_ssim:#{params[:id]} OR elector_id_ssim:#{params[:id]}) AND format_ssim:\"Election Record\"", :fl=>'title_tesim id'},:per_page=>'1000')['response']['docs']
+    docs = Blacklight.solr.get('select', :params=>{:q=>"(candidate_id_ssim:#{params[:id]} OR elector_id_ssim:#{params[:id]}) AND format_ssim:\"Election Record\"", :fl=>'title_tesi id', :per_page=>'1000'})['response']['docs']
     content_tag(:ul) do
       docs.collect { |election|
-        concat(content_tag(:li, link_to(election['title_tesim'].first, catalog_path(election['id']))))
+        concat(content_tag(:li, link_to(election['title_tesi'], catalog_path(election['id']))))
       }
     end
   end
