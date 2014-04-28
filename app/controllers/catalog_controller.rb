85331b739c78200c2a9575021c499ce3156378d1
Mike Korcynski <mkorcy@gmail.com>
2014-04-28 10:58:49 -0400

    update solr fields for new version of af

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -15,13 +15,13 @@ class CatalogController < ApplicationController
     }
 
     # solr field configuration for search results/index views
-    config.index.show_link = 'title_display'
-    config.index.record_display_type = 'format'
+    config.index.title_field = 'title_tesim'
+    config.index.display_type_field  = 'format_ssim'
 
     # solr field configuration for document/show views
-    config.show.html_title = 'title_display'
-    config.show.heading = 'title_display'
-    config.show.display_type = 'format'
+    config.show.title_field  = 'title_tesim'
+    #config.show.heading = 'title_display'
+    config.show.display_type_field  = 'format_ssim'
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
@@ -39,12 +39,12 @@ class CatalogController < ApplicationController
     # on the solr side in the request handler itself. Request handler defaults
     # sniffing requires solr requests to be made with "echoParams=all", for
     # app code to actually have it echo'd back to see it.  
-    config.add_facet_field 'state_name_facet', :label => 'State', :sort => 'index', :limit => 50
-    config.add_facet_field 'date_i', :label => 'Year', :range=>true, :sort => 'index'
-    config.add_facet_field 'office_name_facet', :label => 'Office', :limit => 20 
-    config.add_facet_field 'jurisdiction_facet', :label => 'Jurisdiction', :limit => 15 
-    config.add_facet_field 'party_affiliation_facet', :label => 'Party', :limit => 15 
-    config.add_facet_field 'election_type_facet', :label => 'Election Type'  
+    config.add_facet_field 'state_name_sim', :label => 'State', :sort => 'index', :limit => 2
+    config.add_facet_field 'date_sim', :label => 'Year', :range=>true, :sort => 'index'
+    config.add_facet_field 'office_name_sim', :label => 'Office', :limit => 20
+    config.add_facet_field 'jurisdiction_sim', :label => 'Jurisdiction', :limit => 15
+    config.add_facet_field 'party_affiliation_sim', :label => 'Party', :limit => 15
+    config.add_facet_field 'election_type_sim', :label => 'Election Type'
     config.add_facet_fields_to_solr_request!
 
     # Have BL send all facet field names to Solr, which has been the default
@@ -53,11 +53,11 @@ class CatalogController < ApplicationController
     config.default_solr_params[:'facet.field'] = config.facet_fields.keys
 
     # solr fields to be displayed in the index (search results) view
-    #   The ordering of the field names is the order of the display 
-    config.add_index_field 'office_name_t', :label => 'Office:' 
-    config.add_index_field 'jurisdiction_display', :label => 'Jurisdiction:' 
-    config.add_index_field 'date_t', :label => 'Year:' 
-    config.add_index_field 'state_name_facet', :label => 'State:' 
+    #   The ordering of the field names is the order of the display
+    config.add_index_field 'office_name_tesim', :label => 'Office'
+    config.add_index_field 'jurisdiction_tesim', :label => 'Jurisdiction'
+    config.add_index_field 'date_tesim', :label => 'Year'
+    config.add_index_field 'state_name_tesim', :label => 'State'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display 
@@ -95,10 +95,9 @@ class CatalogController < ApplicationController
     config.add_search_field 'all_fields', :label => 'All Fields'
 
     config.add_search_field('Candidate') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'candidate_name_t' }
       field.solr_local_parameters = { 
-        :qf => 'candidate_name_t',
-        :pf => 'candidate_name_t',
+        :qf => 'name_tesim',
+        :pf => 'name_tesim',
       }
     end
 
@@ -144,9 +143,9 @@ class CatalogController < ApplicationController
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, date_i asc, title_sort asc', :label => 'relevance'
-    config.add_sort_field 'date_i asc, title_sort asc', :label => 'year'
-    config.add_sort_field 'title_sort asc, date_i asc', :label => 'title'
+    config.add_sort_field 'score desc, date_isi asc, title_ssi asc', :label => 'relevance'
+    config.add_sort_field 'date_isi asc, title_ssi asc', :label => 'year'
+    config.add_sort_field 'title_ssi asc, date_isi asc', :label => 'title'
 
     # If there are more than this many search results, no spelling ("did you 
     # mean") suggestion is offered.
@@ -158,7 +157,7 @@ class CatalogController < ApplicationController
   # @param user_parameters the current user-subitted parameters
   def exclude_unwanted_models(solr_parameters, user_parameters)
     solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << "has_model_s:\"info:fedora/cm:VotingRecord\" OR format:Candidate"
+    solr_parameters[:fq] << "has_model_ssim:\"info:fedora/cm:VotingRecord\" OR format_ssim:Candidate"
   end
 
 end 
