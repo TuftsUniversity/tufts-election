167259d92ab0e5beb8c859608b96ddd04b9c7dd9
val99erie <val99erie@gmail.com>
2015-03-09 11:27:01 -0500

    fixed a spec

--- spec/helpers/candidate_helper_spec.rb
+++ spec/helpers/candidate_helper_spec.rb
@@ -4,7 +4,7 @@ describe CandidateHelper do
 
   describe "#list_elections" do
     subject {
-      Blacklight.solr.should_receive(:get).with('select', :params=>{:q=>'(candidate_id_ssim:AA00005 OR elector_id_ssim:AA00005) AND format_ssim:"Election Record"', :fl=>'title_tesi id', :per_page=>"1000"})
+      Blacklight.solr.should_receive(:get).with('select', :params=>{:q=>'(candidate_id_ssim:AA00005 OR elector_id_ssim:AA00005) AND format_ssim:"Election Record"', :fl=>'title_tesi id', :rows=>"1000", :sort=>"title_ssi asc"})
         .and_return({'response'=>{'docs'=>[{'id'=>'one', 'title_tesi'=>'first'}, {'id'=>'two', 'title_tesi'=>'second'}]}})
       params[:id] = 'AA00005'
       helper.list_elections
@@ -13,6 +13,5 @@ describe CandidateHelper do
     it { should == '<ul><li><a href="/catalog/one/track">first</a></li><li><a href="/catalog/two/track">second</a></li></ul>'}
     it { should be_html_safe}
   end
-
 
 end
