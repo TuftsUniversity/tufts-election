78aa9994ca32f897b8dcde271d81d6478dd03f29
Tony Collen <tony@halogenlabs.com>
2015-03-11 16:13:03 -0500

    display state thumbnails if available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,4 +57,69 @@ module ApplicationHelper
     end
   end
 
+  def state_thumbnail(name)
+    if abbr = us_states[name]
+      image_tag "state_images/#{abbr.downcase}.gif", class: "stateThumbnail", alt: "Map of #{name}", title: "Map of #{name}"
+    else
+      ''
+    end
+  end
+
+  def us_states
+    {
+      'Alabama'              => 'AL',
+      'Alaska'               => 'AK',
+      'Arizona'              => 'AZ',
+      'Arkansas'             => 'AR',
+      'California'           => 'CA',
+      'Colorado'             => 'CO',
+      'Connecticut'          => 'CT',
+      'Delaware'             => 'DE',
+      'District of Columbia' => 'DC',
+      'Florida'              => 'FL',
+      'Georgia'              => 'GA',
+      'Hawaii'               => 'HI',
+      'Idaho'                => 'ID',
+      'Illinois'             => 'IL',
+      'Indiana'              => 'IN',
+      'Iowa'                 => 'IA',
+      'Kansas'               => 'KS',
+      'Kentucky'             => 'KY',
+      'Louisiana'            => 'LA',
+      'Maine'                => 'ME',
+      'Maryland'             => 'MD',
+      'Massachusetts'        => 'MA',
+      'Michigan'             => 'MI',
+      'Minnesota'            => 'MN',
+      'Mississippi'          => 'MS',
+      'Missouri'             => 'MO',
+      'Montana'              => 'MT',
+      'Nebraska'             => 'NE',
+      'Nevada'               => 'NV',
+      'New Hampshire'        => 'NH',
+      'New Jersey'           => 'NJ',
+      'New Mexico'           => 'NM',
+      'New York'             => 'NY',
+      'North Carolina'       => 'NC',
+      'North Dakota'         => 'ND',
+      'Ohio'                 => 'OH',
+      'Oklahoma'             => 'OK',
+      'Oregon'               => 'OR',
+      'Pennsylvania'         => 'PA',
+      'Puerto Rico'          => 'PR',
+      'Rhode Island'         => 'RI',
+      'South Carolina'       => 'SC',
+      'South Dakota'         => 'SD',
+      'Tennessee'            => 'TN',
+      'Texas'                => 'TX',
+      'Utah'                 => 'UT',
+      'Vermont'              => 'VT',
+      'Virginia'             => 'VA',
+      'Washington'           => 'WA',
+      'West Virginia'        => 'WV',
+      'Wisconsin'            => 'WI',
+      'Wyoming'              => 'WY'
+    }
+  end
+
 end
