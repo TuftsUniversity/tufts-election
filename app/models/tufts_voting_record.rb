101655024cc8743b543cd691c4ac28dfc9a3773b
Mike Korcynski <mkorcy@gmail.com>
2015-05-04 12:02:44 -0400

    don't index nulls for facet in party

--- app/models/tufts_voting_record.rb
+++ app/models/tufts_voting_record.rb
@@ -7,6 +7,10 @@ class TuftsVotingRecord < ActiveFedora::Base
     solr_doc["format_ssim"] = solr_doc["format_tesim"]
     solr_doc["title_tesi"] = solr_doc["title_ssi"] = solr_doc["title_tesim"].first
     solr_doc["party_affiliation_sim"] = datastreams["RECORD-XML"].office.role.ballot.candidate.affiliation.to_a + datastreams["RECORD-XML"].office.role.ballot.elector.affiliation.to_a
+    affiliate_ids = datastreams["RECORD-XML"].office.role.ballot.candidate.affiliation_id.to_a + datastreams["RECORD-XML"].office.role.ballot.elector.affiliation_id.to_a
+    solr_doc["party_affiliation_id_ssim"] =  affiliate_ids - ['null']#if affiliate_ids - ['null'] == []
+
+
     solr_doc["voting_record_xml_tesi"] = datastreams["RECORD-XML"].to_xml
     all_text_values = []
     solr_doc.each do |key,value|
