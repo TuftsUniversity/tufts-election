d2387a3c3c02e1b3547889c2d2c7b51a5f34c805
Travis Lilleberg <travis.lilleberg@tufts.edu>
2016-07-13 11:48:04 -0400

    Moved semantics from old solr_document model to new. Moved logic from catalog controller to search_builder model.

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
+
+
+  self.default_processor_chain += [:exclude_unwanted_models, :exclude_drafts]
+
+  # This filters out objects that you want to exclude from search results.  By default it only excludes FileAssets
+  # @param solr_parameters the current solr parameters
+  # @param user_parameters the current user-subitted parameters
+  def exclude_unwanted_models(solr_parameters, user_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << "has_model_ssim:\"info:fedora/cm:VotingRecord\" OR format_ssim:Candidate"
+  end
+
+  def exclude_drafts(solr_parameters,user_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << "-id:draft*"
+  end
 end
