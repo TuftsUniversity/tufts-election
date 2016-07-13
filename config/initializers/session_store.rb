366c6d016ed42468afcd57d3b52108e8023d0d37
Travis Lilleberg <travis.lilleberg@tufts.edu>
2016-07-13 09:29:34 -0400

    Ran rake rails:update to update configs. Then painstakingly moved all the custom stuff back in, (hopefully). Also explicitly declared blacklight routing, as per the upgrade instructions.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TuftsElection::Application.config.session_store :cookie_store, key: '_tufts-election_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# TuftsElection::Application.config.session_store :active_record_store
+TuftsElection::Application.config.session_store :cookie_store, key: '_tufts_election_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,81 @@
 ALLOW_DOTS ||= /[a-zA-Z0-9_\-.:]+/
 
 TuftsElection::Application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
 
-  Blacklight.add_routes(self)
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  mount Blacklight::Engine => '/'
+
+  concern :searchable, Blacklight::Routes::Searchable.new
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resource :catalog, only: [:index], controller: 'catalog' do
+    concerns :searchable
+  end
+
+  resources :solr_documents, only: [:show], controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
 
   root :to => "catalog#index"
 
@@ -13,4 +85,5 @@ TuftsElection::Application.routes.draw do
   resources :catalog, :id => ALLOW_DOTS
   resources :candidates, :only=>'index'
   resources :message_queues, :only=>'index'
+
 end
