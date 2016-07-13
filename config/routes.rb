90a3383bb074d05d5b8f56f9d95d22631ee2d9d9
Travis Lilleberg <travis.lilleberg@tufts.edu>
2016-07-13 10:31:28 -0400

    Added via: [:get] explicitly to route. Because the error told me to.

--- config/routes.rb
+++ config/routes.rb
@@ -81,7 +81,7 @@ TuftsElection::Application.routes.draw do
 
   devise_for :users
 
-  match '/catalog/:id/track', :to => 'catalog#track', :constraints => {:id => /.*/}, :as =>'catalog'
+  match '/catalog/:id/track', :to => 'catalog#track', :constraints => {:id => /.*/}, via: [:get], :as =>'catalog'
   resources :catalog, :id => ALLOW_DOTS
   resources :candidates, :only=>'index'
   resources :message_queues, :only=>'index'
