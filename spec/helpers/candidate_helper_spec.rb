3725708999e1bbc6b6a45043a22fe2bfc395f2e2
Travis Lilleberg <travis.lilleberg@tufts.edu>
2016-12-20 14:42:35 -0500

    Fixed CandidateHelper.list_elections test by adding viable fixtures to test for and doubling blacklight_config. Turned off partial double verification to allow mocking of blacklight_config.

--- spec/helpers/candidate_helper_spec.rb
+++ spec/helpers/candidate_helper_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
 describe CandidateHelper do
-  describe "#list_elections" do
-    skip "-- blacklight upgrade broke this" do
-      subject {
-        expect(Blacklight.solr).to receive(:get).with('select', :params=>{:q=>'(candidate_id_ssim:AA00005 OR elector_id_ssim:AA00005) AND format_ssim:"Election Record"', :fl=>'title_tesi id', :fq=>"-id:draft*", :rows=>"1000", :sort=>"title_ssi asc"})
-        .and_return({'response'=>{'docs'=>[{'id'=>'one', 'title_tesi'=>'first'}, {'id'=>'two', 'title_tesi'=>'second'}]}})
-        params[:id] = 'AA00005'
-        helper.list_elections
-      }
-
-      it { is_expected.to eq('<ul><li><a href="/catalog/one/track">first</a></li><li><a href="/catalog/two/track">second</a></li></ul>')}
-      it { is_expected.to be_html_safe}
-    end
+  before(:each) do
+    allow(helper).to receive(:blacklight_config).and_return(Blacklight::Configuration.new)
   end
 
+  describe "#list_elections" do
+    subject {
+      params[:id] = 'AJ0156'
+      helper.list_elections
+    }
+
+    it { is_expected.to eq(
+      '<ul><li><a href="/catalog/tufts:nj.sheriff.hunterdon.1792/track">New Jersey 1792 Sheriff, Hunterdon County</a></li><li><a href="/catalog/tufts:nj.sheriff.hunterdon.1793/track">New Jersey 1793 Sheriff, Hunterdon County</a></li></ul>'
+    )}
+    it { is_expected.to be_html_safe }
+  end
 
 end
