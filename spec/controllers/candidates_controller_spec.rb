049fc6be4fb2243601bfb6be3a58b0487dd39ffc
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-03-29 13:18:06 -0400

    Fixed candidate controller test

--- spec/controllers/candidates_controller_spec.rb
+++ spec/controllers/candidates_controller_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 describe CandidatesController do
-  before { skip("Waiting for migration to finalize") }
   it "should have index" do
-    get :index, :candidate_last_name_letter=>'A'
+    get :index, params: { candidate_last_name_letter: 'A' }
     expect(response).to be_success
   end
 
