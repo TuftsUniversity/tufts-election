3563018a26a30686cad1999ca2a065559f65366c
Mike Korcynski <mkorcy@gmail.com>
2020-02-14 15:14:15 -0500

    TDLR-1193: Candidate pages in NNV don't display election names

--- app/helpers/candidate_helper.rb
+++ app/helpers/candidate_helper.rb
@@ -6,13 +6,13 @@ module CandidateHelper
       qt: "standard",
       q: "(candidate_id_ssim:#{params[:id]} OR elector_id_ssim:#{params[:id]}) AND format_ssim:\"Election Record\"",
       fq: '-id:draft*',
-      fl: 'title_tesi id',
+      fl: 'title_ssi id',
       rows: '1000',
       sort: 'title_ssi asc'
     })[1]
     content_tag(:ul) do
       docs.collect { |election|
-        concat(content_tag(:li, link_to(election['title_tesi'], catalog_path(election['id']))))
+        concat(content_tag(:li, link_to(election['title_ssi'], catalog_path(election['id']))))
       }
     end
   end
