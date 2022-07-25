f8ce6e0dbc34f7c8a04117d426480564b694bc97
Mike Korcynski <mkorcy@gmail.com>
2022-07-25 16:12:08 -0400

    simplecov config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,21 @@
+# frozen_string_literal: true
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  add_filter %w[version.rb initializer.rb]
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
