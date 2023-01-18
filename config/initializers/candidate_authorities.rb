5d7fdbc460e25da564dfbf5e157f72ec6530d43f
Mike Korcynski <mkorcy@gmail.com>
2023-01-18 11:49:05 -0500

    initializer clean ups

--- config/initializers/candidate_authorities.rb
+++
@@ -1,3 +0,0 @@
-# Import candidate names authority
-
-# This functionality has been moved to script/import_candidates. Run that script if you need the candidate authorities.
--- config/initializers/state_authorities.rb
+++ config/initializers/state_authorities.rb
@@ -1,29 +1,13 @@
 # Import the state authority data
 
 require 'net/http'
-uri_for_state_file = 'https://dl.tufts.edu/file_assets/generic/tufts:state-authority/0'
 
 filename = Rails.root.join('tmp', 'state-authorities.xml')
 
-if (Rails.env.development? || Rails.env.test?) && File.exist?(filename)
-  Rails.logger.info "Skipping download, using existing state authority: #{filename}"
-  puts "Skipping download, using existing state authority: #{filename}"
+if File.exist?(filename)
+  Rails.logger.info "Using existing states authority: #{filename}"
 else
-  Rails.logger.info "Downloading state authority from #{uri_for_state_file}"
-  puts "Downloading state authority from #{uri_for_state_file}"
-
-  response = Net::HTTP.get_response(URI(uri_for_state_file))
-
-  if response.code == '200'
-    File.open(filename, 'w') do |file|
-      file.write response.body.force_encoding('UTF-8')
-    end
-  else
-    msg = "Warning: Could not load #{uri_for_state_file} (HTTP response: #{response.code}). Attempting to read local cached copy."
-
-    puts msg
-    Rails.logger.warn msg
-  end
+  Rails.logger.warn "ERROR: State authority file missing."
 end
 
 # Load the states
