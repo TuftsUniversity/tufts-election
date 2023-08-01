6f2205b0e227a99e48b05893aa278dbc5e8b0c46
Ryan Orlando <rynorlando@gmail.com>
2023-08-01 09:28:00 -0400

    Increase code coverage for application helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -14,4 +14,35 @@ describe ApplicationHelper do
       expect(helper.office_name('unknown id')).to eq 'unknown id'
     end
   end
+
+  describe '#party_name' do
+    let(:party) { { id: 'A123', name: 'Foo party'} }
+    before { Party.register(party) }
+
+    it 'returns the name of the office' do
+      expect(helper.party_name(party[:id])).to eq party[:name]
+    end
+
+    it 'returns the input id if the office is not found' do
+      expect(helper.party_name('unknown id')).to eq 'unknown id'
+    end
+  end
+
+  describe '#state_thumbnail' do
+    it 'return empty if it is not given a known state' do
+      expect(helper.state_thumbnail('Ireland')).to eq ''
+    end
+
+    it 'returns an image tag' do
+      alaska_image_tag = helper.state_thumbnail('Alaska')
+      expect(alaska_image_tag).to include 'Alaska'
+      expect(alaska_image_tag).to include '.gif'
+    end
+  end
+
+  describe '#us_states' do
+    it 'has all 50 states' do
+      expect(helper.us_states.length()).to be >= 50
+    end
+  end
 end
