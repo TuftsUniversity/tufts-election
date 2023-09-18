e7e01951afb72534028a20abc3ed941acbdd05ef
Ryan Orlando <rynorlando@gmail.com>
2023-09-18 11:57:13 -0400

    please work

--- spec/controllers/candidates_controller_spec.rb
+++ spec/controllers/candidates_controller_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-describe CandidatesController do
+describe CandidatesController, api: true do
   it "should have index" do
     get :index, params: { candidate_last_name_letter: 'A' }
     expect(response).to be_success
