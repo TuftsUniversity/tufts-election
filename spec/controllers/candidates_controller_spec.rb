61d7c6f2d94833c6de7b67aee72929387644b79d
Ryan Orlando <rynorlando@gmail.com>
2023-09-18 09:13:29 -0400

    try moving the spec

--- spec/controllers/candidates_controller_spec.rb
+++ spec/controllers/candidates_controller_spec.rb
@@ -6,4 +6,20 @@ describe CandidatesController do
     get :index, params: { candidate_last_name_letter: 'A' }
     expect(response).to be_success
   end
+
+  describe "#list_elections" do
+    subject do
+      params[:id] = 'AJ0156'
+      model_instance.controller.list_elections
+    end
+
+    it {
+      is_expected.to eq(
+      '<ul><li><a href="/catalog/4j03d0249/track">New Jersey 1792 Sheriff, Hunterdon County</a></li><li><a href="/catalog/vt150j84n/track">New Jersey 1793 Sheriff, Hunterdon County</a></li></ul>'
+    )
+    }
+    it { is_expected.to be_html_safe }
+  end
 end
+
+
--- spec/helpers/candidate_helper_spec.rb
+++ spec/helpers/candidate_helper_spec.rb
@@ -39,17 +39,17 @@ describe CandidateHelper do
     allow(model_instance).to receive(:blacklight_config).and_return(Blacklight::Configuration.new)
   end
 
-  describe "#list_elections" do
-    subject do
-      params[:id] = 'AJ0156'
-      model_instance.controller.list_elections
-    end
+  # describe "#list_elections" do
+  #   subject do
+  #     params[:id] = 'AJ0156'
+  #     model_instance.controller.list_elections
+  #   end
 
-    it {
-      is_expected.to eq(
-      '<ul><li><a href="/catalog/4j03d0249/track">New Jersey 1792 Sheriff, Hunterdon County</a></li><li><a href="/catalog/vt150j84n/track">New Jersey 1793 Sheriff, Hunterdon County</a></li></ul>'
-    )
-    }
-    it { is_expected.to be_html_safe }
-  end
+  #   it {
+  #     is_expected.to eq(
+  #     '<ul><li><a href="/catalog/4j03d0249/track">New Jersey 1792 Sheriff, Hunterdon County</a></li><li><a href="/catalog/vt150j84n/track">New Jersey 1793 Sheriff, Hunterdon County</a></li></ul>'
+  #   )
+  #   }
+  #   it { is_expected.to be_html_safe }
+  # end
 end
