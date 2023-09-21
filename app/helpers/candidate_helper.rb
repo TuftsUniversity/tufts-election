594dd153288abe9a44a2bb03d6ce996fcf66b617
Ryan Orlando <rynorlando@gmail.com>
2023-09-21 15:48:29 -0400

    search works ?

--- app/helpers/candidate_helper.rb
+++ app/helpers/candidate_helper.rb
@@ -41,7 +41,16 @@ module CandidateHelper
 
       #return "class: #{self.class} \n methods: #{self.methods} \n search_session methods: #{self.search_session.methods} "
 
-      docs = new_search_state.fetch([])
+      docs = new_search_state.search_results({
+        qt: "standard",
+        q: "(candidate_id_ssim:#{params[:id]} OR elector_id_ssim:#{params[:id]}) AND format_ssim:\"Election Record\"",
+        fq: '-id:draft*',
+        fl: 'title_ssi id',
+        rows: '1000',
+        sort: 'title_ssi asc'
+      })
+
+      #docs = new_search_state.fetch([])
       # params = {
       #   qt: "standard",
       #   q: "(candidate_id_ssim:#{params[:id]} OR elector_id_ssim:#{params[:id]}) AND format_ssim:\"Election Record\"",
